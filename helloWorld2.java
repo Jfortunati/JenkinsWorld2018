7a99b1e37f6baa32fa07f6dcec36af233b0777d7
Jfortunati <Jonathan.Fortunati@smartbear.com>
2018-09-06 11:01:28 -0400

    Update helloWorld2.java

--- helloWorld2.java
+++ helloWorld2.java
@@ -2,8 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-		//Edit commit2 - add some changes
-		// Edit file 2 in commit 3
         System.out.println("Hello, World");
     }
 
